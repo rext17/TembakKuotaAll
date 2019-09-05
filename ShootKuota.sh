5f5e435b4ad91bb35610be2575a07c4de971f21a
CyberArmyAttack <54892703+CyberArmyAttack@users.noreply.github.com>
2019-09-05 16:50:42 +0700

    ShootKuota.sh
    Apa lu liat liat ashu

---
+++ ShootKuota.sh
@@ -0,0 +1,96 @@
+#!/system/xbin/bash
+#Bersihkan Layar
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+sleep 1
+echo "\033[32;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+toilet -f standard -F gay "Tembak Kuota"
+echo "\033[31;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+echo ""
+echo "Author : " Ivan Cruzz Revoer"
+" Team : "  CyberArmyAttack"
+echo " Kontak Me : " 0857xxxx"
+" Pesan : "Jangan Di Salah Gunakan Dan Disebar :) Karena Nanti Dosa Tanggung Sendiri:)"
+sleep 1
+echo "ThanksTo:"
+echo "Allmember CyberArmyAttack"
+echo "\033[33;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+echo ""
+sleep 2
+echo "Mau Tembak Kuota...""Silahkan Pilih No Dibawah Bacaan Ini !"
+sleep 0.2
+echo "1.XL/AXIS"
+sleep 0.2
+echo "2.INDOSAT"
+sleep 0.2
+echo "3.SIMPATI"
+sleep 0.2
+echo "4.SMARTFREN"
+sleep 0.2
+echo "5.3(Three)"
+sleep 0.2
+echo "masukkan kodenya"
+read p;
+echo "\033[31;1mSedang Menuju Proses..."
+sleep 1
+echo "\033[37;1m█10%"
+sleep 2
+echo "██20%"
+sleep 3
+echo "███30%"
+sleep 4
+echo "████40%"
+sleep 5
+echo "█████50%"
+sleep 4
+echo "██████60%"
+sleep 3
+echo "███████70%"
+sleep 2
+echo "████████80%"
+sleep 1
+echo "█████████90%"
+sleep 2
+echo "██████████100%"
+sleep 3
+clear
+
+echo "\033[33;1mMasukkan Nomer Anda Yang Benar...Contoh:0857xxxx"
+sleep 2
+echo "Isi Dibawah!"
+read p;
+clear
+
+echo "\033[33;1mSilahkan Pilih Dari Daftar Menu Dibawah Ini!"
+sleep 2
+echo "\033[35;1m1.Paket UNLIMITED INDOSAT"
+sleep 2
+echo "2.Paket UNLIMITED SMARTFREN"
+sleep 2
+echo "3.Paket UNLIMITED AXIS/XL"
+sleep 2
+echo "4.Paket UNLIMITED SIMPATI/TELKOMSEL"
+sleep 2
+echo "5.Paket UNLIMITED 3(Three)"
+sleep 2
+echo "masukkan Kodenya..."
+read p;
+clear
+
+echo "\033[33;1mTunggulah Sekitar 10 Menit Untuk Mendapatkan Sms Masuk...."
+sleep 1
+echo "Jika 10Menit Belum Ada Sms Masuk Berarti No Kamu tidak Valid...!!"
+sleep 2
+echo "Thanks To:MrRevoer'S - MrIvanId - CyberArmyAttack"
+sleep 2
+echo "Pesan:Jangan Salahin Admin Ya Kentod...Mau Kuota Gratis Ga Sabar..."
+sleep 1
+echo "Kuota Berhasil Ditembakan Silahkan Cek Nomor Anda!!"
+sleep 2
+
